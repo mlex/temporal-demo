1dfbd4005057e1cbb64a1476fac459953d0a655e
Michael Lex <michael.lex@clickhouse.com>
2024-07-10 10:48:14 +0200

    Initial commit

---
+++ worker/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"log/slog"
+	"os"
+
+	"github.com/temporalio/samples-go/helloworld"
+	"go.temporal.io/sdk/client"
+	tlog "go.temporal.io/sdk/log"
+	"go.temporal.io/sdk/worker"
+)
+
+func main() {
+	// The client and worker are heavyweight objects that should be created once per process.
+	c, err := client.Dial(client.Options{
+		Logger: tlog.NewStructuredLogger(
+			slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+				AddSource: true,
+				Level:     slog.LevelDebug,
+			}))),
+	})
+	if err != nil {
+		log.Fatalln("Unable to create client", err)
+	}
+	defer c.Close()
+
+	w := worker.New(c, "hello-world", worker.Options{})
+
+	w.RegisterWorkflow(helloworld.MyWorkflow)
+	w.RegisterActivity(helloworld.MyActivity)
+
+	err = w.Run(worker.InterruptCh())
+	if err != nil {
+		log.Fatalln("Unable to start worker", err)
+	}
+}
